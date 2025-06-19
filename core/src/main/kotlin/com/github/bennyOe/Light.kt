f2b0609daafe7f4fef0bda642c9ac91f96f26f69
Benjamin Oechsle <benjamin.oechsle@lotto24.de>
2025-06-19 07:21:34 +0200

    box2dlight first implementation

--- core/src/main/kotlin/com/github/bennyOe/Light.kt
+++ core/src/main/kotlin/com/github/bennyOe/Light.kt
@@ -1,9 +1,13 @@
 package com.github.bennyOe
 
+import box2dLight.ConeLight
+import box2dLight.PointLight
+import box2dLight.RayHandler
 import com.badlogic.gdx.Gdx
 import com.badlogic.gdx.Input
 import com.badlogic.gdx.InputAdapter
 import com.badlogic.gdx.files.FileHandle
+import com.badlogic.gdx.graphics.Color
 import com.badlogic.gdx.graphics.GL20
 import com.badlogic.gdx.graphics.OrthographicCamera
 import com.badlogic.gdx.graphics.Texture
@@ -11,16 +15,25 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch
 import com.badlogic.gdx.graphics.glutils.ShaderProgram
 import com.badlogic.gdx.math.Vector3
 import com.badlogic.gdx.math.Vector4
+import com.badlogic.gdx.physics.box2d.Body
+import com.badlogic.gdx.physics.box2d.BodyDef
+import com.badlogic.gdx.physics.box2d.Box2DDebugRenderer
+import com.badlogic.gdx.physics.box2d.FixtureDef
+import com.badlogic.gdx.physics.box2d.PolygonShape
+import com.badlogic.gdx.physics.box2d.World
 import com.badlogic.gdx.utils.GdxRuntimeException
 import ktx.app.KtxScreen
 import ktx.assets.disposeSafely
 import ktx.graphics.use
+import ktx.math.vec2
 import ktx.math.vec3
 import ktx.math.vec4
 import java.lang.Math.toDegrees
 import java.lang.Math.toRadians
 import kotlin.math.acos
 import kotlin.math.cos
+
+const val PPM = 100f
 
 class Light : KtxScreen {
     private lateinit var batch: SpriteBatch
@@ -43,16 +56,23 @@ class Light : KtxScreen {
     private val falloff = vec3(.4f, 3f, 20f)
     private val vertShader: FileHandle = Gdx.files.internal("shader/light.vert")
     private val fragShader: FileHandle = Gdx.files.internal("shader/light.frag")
-    private val activeLights: MutableList<LightData> = mutableListOf(
-//        LightData(lightType, lightPos,falloff, lightColor,ambientColor,falloff,coneAngleValue),
-    )
+    private val activeLights: MutableList<LightData> = mutableListOf()
+
+    // --------- Box 2d lights --------//
+    private val world = World(vec2(0f, -9.81f), true)
+    private val rayHandler = RayHandler(world)
+    private lateinit var pointLight: ConeLight
+    private lateinit var wallBody: Body
+    private val debugRenderer = Box2DDebugRenderer()
 
 
     override fun show() {
-        val screenWidth = Gdx.graphics.width.toFloat()
-        val screenHeight = Gdx.graphics.height.toFloat()
-        val spacing = screenWidth / 10f
+        rayHandler.setShadows(true)
+        rayHandler.setAmbientLight(0f, 0f, 0f, 0.5f)
+        rayHandler.setBlurNum(3)
+        createWallBody(world)
 
+        // ---- END BOX2d LIGHTS-------//
         activeLights.add(
             LightData(
                 lightType,
@@ -94,11 +114,13 @@ class Light : KtxScreen {
             shader.setUniformf("ConeAngle$prefix", light.coneAngle)
         }
 
-        batch = SpriteBatch(1000, shader)
-        batch.shader = shader
+        batch = SpriteBatch(1000)
+        batch.shader = null
 
-        cam = OrthographicCamera(Gdx.graphics.width.toFloat(), Gdx.graphics.height.toFloat())
-        cam.setToOrtho(false)
+        cam = OrthographicCamera( Gdx.graphics.width.toFloat() / PPM, Gdx.graphics.height.toFloat() / PPM)
+        val centerX = cam.position.x
+        val centerY = cam.position.y
+        pointLight = ConeLight(rayHandler, 128, Color(1f, 1f, 1f, 1f), 8f, centerX +2f , centerY +3f, 240f, 40f)
 
         Gdx.input.inputProcessor = object : InputAdapter() {
             override fun scrolled(amountX: Float, amountY: Float): Boolean {
@@ -110,33 +132,50 @@ class Light : KtxScreen {
     }
 
     override fun resize(width: Int, height: Int) {
-        cam.setToOrtho(false, width.toFloat(), height.toFloat())
+        cam.setToOrtho(false, width / PPM, height / PPM)
+
+        // ZENTRIEREN auf Weltmitte!
+        cam.position.set(
+            0f,
+            0f,
+            0f
+        )
+
+        cam.update()
         batch.projectionMatrix = cam.combined
         shader.bind()
         shader.setUniformf("Resolution", width.toFloat(), height.toFloat())
+        rayHandler.setCombinedMatrix(cam)
     }
 
     override fun render(delta: Float) {
+        world.step(1/60f, 6, 2)
         println(Gdx.graphics.framesPerSecond)
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT)
 
-        batch.use {
-            val x = Gdx.input.x.toFloat() / Gdx.graphics.width
-            val y = 1f - Gdx.input.y.toFloat() / Gdx.graphics.height
-
-            lightPos.set(x, y, 0f)
-            shader.setUniformf("LightPos", lightPos)
-
-            val lightDir = vec3(-1f, -1f, 0f)
-            shader.setUniformf("LightDir", lightDir)
-
-            keyboardInput()
-
-            wallNormals.bind(1)
-            wall.bind(0)
-
-            batch.draw(wall, 0f, 0f)
-        }
+//        batch.use {
+//            val x = Gdx.input.x.toFloat() / Gdx.graphics.width
+//            val y = 1f - Gdx.input.y.toFloat() / Gdx.graphics.height
+//
+//            lightPos.set(x, y, 0f)
+//            shader.setUniformf("LightPos", lightPos)
+//
+//            val lightDir = vec3(-1f, -1f, 0f)
+//            shader.setUniformf("LightDir", lightDir)
+//
+//            keyboardInput()
+//
+//            wallNormals.bind(1)
+//            wall.bind(0)
+//
+//            batch.draw(wall, 0f, 0f)
+//        }
+        //------ Box 2d lights ------//
+        cam.update()
+        debugRenderer.render(world, cam.combined)
+        rayHandler.setShadows(true)
+        rayHandler.setCombinedMatrix(cam)
+        rayHandler.updateAndRender()
     }
 
     private fun keyboardInput() {
@@ -182,6 +221,7 @@ class Light : KtxScreen {
             lightType = LightType.SPOT
         }
         shader.setUniformi("LightType", lightType.ordinal)
+
     }
 
     override fun dispose() {
@@ -189,6 +229,29 @@ class Light : KtxScreen {
         wall.disposeSafely()
         wallNormals.disposeSafely()
         shader.disposeSafely()
+    }
+
+    fun createWallBody(world: World) {
+        val bodyDef = BodyDef().apply {
+            type = BodyDef.BodyType.StaticBody
+            position.set(0f, 0f) // sichtbar im Zentrum
+        }
+
+        wallBody = world.createBody(bodyDef)
+
+        val shape = PolygonShape().apply {
+            setAsBox(1f, 1f, vec2(0f, 0f), 0f) // große Fläche = sichtbar, zentriert
+        }
+
+        val fixtureDef = FixtureDef().apply {
+            this.shape = shape
+            density = 1f
+            friction = 0.5f
+            restitution = 0.1f
+        }
+
+        wallBody.createFixture(fixtureDef)
+        shape.dispose()
     }
 }
 
